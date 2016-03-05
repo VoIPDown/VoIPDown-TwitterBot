91050c82958721a200e51fd4674a948917de2ae8
zakelfassi <zakelfassi@gmail.com>
2016-03-05 01:27:36 -0800

    Add more samples.

--- code/VoIPDown_MA.rb
+++ code/VoIPDown_MA.rb
@@ -11,7 +11,15 @@ tweets = [
   "Hey #USER#! did you know that VoIP was blocked by ANRT (host of #ICANN55) #{days_blocked} days ago? Learn more at http://voipdown.com #VoIPDown",
   "#USER#, did you know that ALL VoIP comm were banned by ANRT (host of #ICANN55) #{days_blocked} days ago? Learn more at http://voipdown.com",
   "Hi #USER#! #{days_blocked} days ago ANRT (host of #ICANN55) banned all VoIP comm in Morocco. Learn more at http://voipdown.com #VoIPDown",
-  "Hi #USER#! #{days_blocked} days ago ANRT (host of #ICANN55) blocked all VoIP comm in Morocco. Learn more at http://voipdown.com #VoIPDown"
+  "Hey #USER#! #{days_blocked} days ago ANRT (host of #ICANN55) blocked all VoIP comm in Morocco. Learn more at http://voipdown.com #VoIPDown",
+  "Hi #USER#! #{days_blocked} days ago ANRT, host of #ICANN55, banned VoIP in Morocco like... North Corea did ... #VoIPDown",
+  "Hey #USER#! #{days_blocked} days ago ANRT, host of #ICANN55, banned VoIP in Morocco like... Pakistan did ... #VoIPDown",
+  "Hi #USER#! #{days_blocked} days ago ANRT, host of #ICANN55, banned Whatsapp calls and said it needs a licence! #VoIPDown",
+  "Hey #USER#! #{days_blocked} days ago ANRT, host of #ICANN55, banned Whatsapp calls and said it needs a licence! #VoIPDown",
+  "Hi #USER#! #{days_blocked} days ago ANRT, host of #ICANN55, banned Facetime and said it needs a licence! #VoIPDown",
+  "Hey #USER#! #{days_blocked} days ago ANRT, host of #ICANN55, decided to illegally to bann VoIP in Morocco?",
+  "Did you know #USER# that, ANRT, host of #ICANN55, proposed to use @Skype for #ICANN55 but banned it for 35M Moroccans...?",
+  "Did you know #USER# that ANRT, host of #ICANN55, banned Whatsapp, Facetime and Skype in #Morocco?"
 ]
 
 twitter_search_string = "#icann55 OR icann55"
